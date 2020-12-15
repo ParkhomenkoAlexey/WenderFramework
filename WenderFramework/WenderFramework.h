5df6408a71adcbbabc9177e367741c41f71c44d6
ParkhomenkoAlexey <parkhomenko_ad@mail.ru>
2020-12-15 16:14:02 +0500

    Initial Commit

---
+++ WenderFramework/WenderFramework.h
@@ -0,0 +1,18 @@
+//
+//  WenderFramework.h
+//  WenderFramework
+//
+//  Created by Пархоменко Алексей on 15.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WenderFramework.
+FOUNDATION_EXPORT double WenderFrameworkVersionNumber;
+
+//! Project version string for WenderFramework.
+FOUNDATION_EXPORT const unsigned char WenderFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WenderFramework/PublicHeader.h>
+
+
